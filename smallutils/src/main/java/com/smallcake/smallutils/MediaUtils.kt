9c2ed19ab3d528e7b3b81dfac9225f8f7e2004b8
xiaoshubin <495303648@qq.com>
2021-06-16 11:14:43 +0800

    MediaUtils播放mp4

--- smallutils/src/main/java/com/smallcake/smallutils/MediaUtils.kt
+++ smallutils/src/main/java/com/smallcake/smallutils/MediaUtils.kt
@@ -1,7 +1,10 @@
 package com.smallcake.smallutils
 
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.view.SurfaceHolder
 
 
 object MediaUtils{
@@ -12,5 +15,24 @@ object MediaUtils{
     fun playMp3(name: String, cls: Class<*>) {
         val resId: Int = ResourceUtils.findResId(name.replace(".mp3", ""), cls)
         MediaPlayer.create(SmallUtils.context, resId).start()
+    }
+
+    /**
+     * 播放raw文件下的 mp4
+     * @param name String 本地视频名称
+     * @param cls Class<*>
+     * @param holder SurfaceHolder
+     */
+    fun playMp4(context: Context, holder: SurfaceHolder,name: String){
+        val resId: Int = ResourceUtils.findResId(name.replace(".mp4", ""), R.raw::class.java)
+        val mPlayer = MediaPlayer.create(context, resId)
+        val audioAttr = AudioAttributes.Builder()
+            .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+            .setLegacyStreamType(AudioManager.STREAM_ALARM)
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MOVIE)
+            .build()
+        mPlayer.setAudioAttributes(audioAttr)
+        mPlayer.setDisplay(holder)
     }
 }
