cc460b4df0b1d68f31c55745fca0d2ebc76c1012
xiaoshubin <495303648@qq.com>
2021-06-16 13:38:51 +0800

    VideoView播放在线mp4

--- smallutils/src/main/java/com/smallcake/smallutils/MediaUtils.kt
+++ smallutils/src/main/java/com/smallcake/smallutils/MediaUtils.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import android.view.SurfaceHolder
+import android.widget.MediaController
+import android.widget.VideoView
 
 
 object MediaUtils{
@@ -18,14 +21,15 @@ object MediaUtils{
     }
 
     /**
-     * 播放raw文件下的 mp4
-     * @param name String 本地视频名称
-     * @param cls Class<*>
-     * @param holder SurfaceHolder
+     * 使用MediaPlayer播放raw文件下的 mp4到SurfaceView
+     * @param context Context
+     * @param holder SurfaceHolder 外部页面SurfaceView提供
+     * 开始播放 mPlayer.start()
+     * 暂停播放 mPlayer.pause()
+     * 结束播放 mPlayer.stop()
      */
-    fun playMp4(context: Context, holder: SurfaceHolder,name: String){
-        val resId: Int = ResourceUtils.findResId(name.replace(".mp4", ""), R.raw::class.java)
-        val mPlayer = MediaPlayer.create(context, resId)
+    fun playMp4(context: Context, holder: SurfaceHolder){
+        val mPlayer = MediaPlayer.create(context, R.raw.lesson)
         val audioAttr = AudioAttributes.Builder()
             .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
             .setLegacyStreamType(AudioManager.STREAM_ALARM)
@@ -34,5 +38,21 @@ object MediaUtils{
             .build()
         mPlayer.setAudioAttributes(audioAttr)
         mPlayer.setDisplay(holder)
+        mPlayer.start()
+    }
+
+    /**
+     * 使用VideoView播放在线视频
+     * @param context Context
+     * @param videoView VideoView
+     */
+    fun playVideo(context: Context,videoView: VideoView){
+        val videoUrl = "http://poss.videocloud.cns.com.cn/oss/2020/07/19/chinanews/MEIZI_YUNSHI/onair/F1B171FB2ECB4319ADAC3FF2915C7E4B.mp4"
+        val mediaController = MediaController(context)
+        mediaController.setAnchorView(videoView)
+        val video: Uri = Uri.parse(videoUrl)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(video)
+        videoView.start()
     }
 }
