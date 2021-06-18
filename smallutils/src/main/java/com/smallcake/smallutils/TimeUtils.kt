44ab4e6e15fbf75f4d08d8c191a28ec50f1131cb
xiaoshubin <495303648@qq.com>
2021-06-18 14:05:28 +0800

    时间工具新增根据时间戳获取星期的方法

--- smallutils/src/main/java/com/smallcake/smallutils/TimeUtils.kt
+++ smallutils/src/main/java/com/smallcake/smallutils/TimeUtils.kt
@@ -16,10 +16,12 @@ object TimeUtils {
      * 年-月-日
      */
     const val YYYY_MM_DD = "yyyy-MM-dd"
+
     /**
      * 24小时 - 时：分：秒
      */
     const val H24_MM_SS = "HH:mm:ss"
+
     /**
      * 12小时 - 时：分：秒
      */
@@ -29,6 +31,7 @@ object TimeUtils {
      * 24小时，年-月-日 时：分：秒
      */
     const val YYYY_MM_DD_H24_MM_SS = "yyyy-MM-dd HH:mm:ss"
+
     /**
      * 24小时，年-月-日 时：分：秒 : 毫秒
      */
@@ -40,7 +43,7 @@ object TimeUtils {
      * @param timeFormat 时间戳 默认yyyy-MM-dd
      * @return
      */
-    fun timeToStr(time: Int,timeFormat:String= YYYY_MM_DD): String {
+    fun timeToStr(time: Int, timeFormat: String = YYYY_MM_DD): String {
         val fm =
             SimpleDateFormat(timeFormat, Locale.CHINA)
         val time1000 = time.toString().toLong() * 1000
@@ -52,11 +55,11 @@ object TimeUtils {
      * @param timeFormat 时间戳 默认yyyy-MM-dd
      * @return 2017-08-14
      */
-     fun today(timeFormat:String= YYYY_MM_DD): String{
+    fun today(timeFormat: String = YYYY_MM_DD): String {
         val date = Date()
         val sdf = SimpleDateFormat(timeFormat, Locale.CHINA)
         return sdf.format(date)
-     }
+    }
 
 
     /**
@@ -64,13 +67,14 @@ object TimeUtils {
      * @return 1502697135
      */
     val currentTime: Int
-      get() = (System.currentTimeMillis() / 1000).toInt()
+        get() = (System.currentTimeMillis() / 1000).toInt()
+
     /**
      * 获取当前系统的时间戳(13位) 毫秒级
      * @return 1502697135000
      */
     val currentTimeSSS: Long
-      get() = System.currentTimeMillis() / 1000L
+        get() = System.currentTimeMillis() / 1000L
 
     /**
      * 获取几个月后的今天
@@ -78,13 +82,68 @@ object TimeUtils {
      * @param addMmonth Int 追加的月份
      * @return String 2021-08-12
      */
-    fun getAddMonthDateFromNow(addMmonth: Int, timeFormat:String= YYYY_MM_DD): String {
+    fun getAddMonthDateFromNow(addMmonth: Int, timeFormat: String = YYYY_MM_DD): String {
         val sdf = SimpleDateFormat(timeFormat, Locale.CHINA)
         val now = Date()
         val calendar = Calendar.getInstance()
         calendar.time = now
         calendar.add(Calendar.MONTH, addMmonth)
         return sdf.format(calendar.time)
+    }
+
+    /**
+     * 将字符串转为时间戳
+     * @param dateFormat 时间戳格式
+     * @param dateTime       时间
+     * @return 时间戳(毫秒)
+     */
+    fun strToDate(dateTime: String, dateFormat: String = YYYY_MM_DD_H24_MM_SS): Int {
+        val sdf = SimpleDateFormat(dateFormat, Locale.CHINA)
+        var date: Date? = null
+        try {
+            date = sdf.parse(dateTime)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return (date?.time?:0L/1000L).toInt()
+    }
+
+    /**
+     * 获取日期是星期几
+     * @param dateTime       时间
+     * @param dateFormat 时间戳格式
+     *
+     * 注意：dateTime格式要为 yyyy-MM-dd
+     */
+    fun getDayofWeek(dateTime: String, dateFormat: String = YYYY_MM_DD): String {
+        val cal = Calendar.getInstance()
+        val sdf = SimpleDateFormat(dateFormat, Locale.CHINA)//这里的格式要和传进来的一样，否则会转换错误
+        var date: Date? = null
+        try {
+            date = sdf.parse(dateTime)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        cal.time = Date(date?.time ?: 0)
+        return when (cal.get(Calendar.DAY_OF_WEEK)) {
+            1 -> "星期日"
+            2 -> "星期一"
+            3 -> "星期二"
+            4 -> "星期三"
+            5 -> "星期四"
+            6 -> "星期五"
+            7 -> "星期六"
+            else -> ""
+        }
+    }
+    /**
+     * 获取几天后的时间戳（秒）
+     * @param day 几天后
+     */
+    fun getTimeDaysLater(day: Int): Int {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.SECOND, calendar.get(Calendar.SECOND) + (day * 60 * 60 * 24)) //秒
+        return (calendar.time.time/1000L).toInt()
     }
 
 
