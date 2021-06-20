87f25641cca404617e2529917d78a9a0f8d7b1d3
xiaoshubin <495303648@qq.com>
2021-06-20 10:31:45 +0800

    新增GsonUtils工具类

---
+++ smallutils/src/main/java/com/smallcake/smallutils/GsonUtils.kt
@@ -0,0 +1,34 @@
+package com.smallcake.smallutils
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+
+/**
+ * Date:2021/6/20 10:11
+ * Author:SmallCake
+ * Desc:Gson工具类
+ **/
+object GsonUtils {
+    /**
+     * 字符串json格式化
+     * @param content String
+     * @return String
+     */
+     fun formatJson(content: String): String {
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val jsonParser = JsonParser()
+        val jsonElement = jsonParser.parse(content)
+        return gson.toJson(jsonElement)
+    }
+
+    /**
+     * 打印任何对象的json
+     * @param param List<Any>?
+     */
+    fun printList(param:List<Any>?){
+        var jsonString = formatJson(Gson().toJson(param))
+        Log.i("GsonUtils",jsonString)
+    }
+}
