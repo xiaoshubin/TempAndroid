e3e2a4727563440200b917eca20676f620a591ce
xiaoshubin <495303648@qq.com>
2021-07-17 15:51:13 +0800

    只关注文本改变后的EditText扩展函数

--- smallutils/src/main/java/com/smallcake/smallutils/EditTextUtils.kt
+++ smallutils/src/main/java/com/smallcake/smallutils/EditTextUtils.kt
@@ -165,3 +165,18 @@ object EditTextUtils {
         })
     }
 }
+
+fun EditText.addAfterTextChangeListener(cb:(Editable?)->Unit){
+    this.addTextChangedListener(object :TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            cb.invoke(s)
+        }
+
+    })
+}
