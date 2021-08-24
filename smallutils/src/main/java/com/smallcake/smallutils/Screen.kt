aaf622dee069b495d92885181cecd7e940caf3f0
xiaoshubin <495303648@qq.com>
2021-08-24 09:23:03 +0800

    新增获取底部导航栏高度

--- smallutils/src/main/java/com/smallcake/smallutils/Screen.kt
+++ smallutils/src/main/java/com/smallcake/smallutils/Screen.kt
@@ -1,12 +1,15 @@
 package com.smallcake.smallutils
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Rect
 import android.util.DisplayMetrics
 import android.view.View
 import android.view.View.MeasureSpec
+import android.view.ViewConfiguration
 import android.view.WindowManager
 import androidx.annotation.FloatRange
 
@@ -98,9 +101,6 @@ object Screen {
         return  b
     }
 
-
-
-
     /**
      * 设置屏幕透明度
      * @param context
@@ -110,5 +110,62 @@ object Screen {
         val lp = context.window.attributes
         lp.alpha = alpha //0.0-1.0
         context.window.attributes = lp
+    }
+    /**
+     * 获取底部菜单栏高度
+     */
+    val navigationBarHeight: Int
+        get() {
+            var result = 0
+            if (hasNavBar()) {
+                val res: Resources = SmallUtils.context!!.resources
+                val resourceId: Int = res.getIdentifier("navigation_bar_height", "dimen", "android")
+                if (resourceId > 0) {
+                    result = res.getDimensionPixelSize(resourceId)
+                }
+            }
+            return result
+        }
+
+
+    /**
+     * 检查是否存在虚拟按键栏
+     *
+     * @return
+     */
+    private fun hasNavBar(): Boolean {
+        val res = SmallUtils.context!!.resources
+        val resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android")
+        return if (resourceId != 0) {
+            var hasNav = res.getBoolean(resourceId)
+            // check override flag
+            val sNavBarOverride: String? = getNavBarOverride()
+            if ("1" == sNavBarOverride) {
+                hasNav = false
+            } else if ("0" == sNavBarOverride) {
+                hasNav = true
+            }
+            hasNav
+        } else { // fallback
+            !ViewConfiguration.get(SmallUtils.context!!).hasPermanentMenuKey()
+        }
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     *
+     */
+    @SuppressLint("PrivateApi")
+    private fun getNavBarOverride(): String? {
+        var sNavBarOverride: String? = null
+        try {
+            val c = Class.forName("android.os.SystemProperties")
+            val m = c.getDeclaredMethod("get", String::class.java)
+            m.isAccessible = true
+            sNavBarOverride = m.invoke(null, "qemu.hw.mainkeys") as String
+        } catch (e: Throwable) {
+        }
+
+        return sNavBarOverride
     }
 }
