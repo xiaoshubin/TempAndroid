ef390296df537558260446db05f37bac4edff824
xiaoshubin <495303648@qq.com>
2022-09-19 11:28:49 +0800

    版本升级，引入ViewPager2作为页面切换器

--- app/src/main/java/com/smallcake/temp/MainActivity.kt
+++ app/src/main/java/com/smallcake/temp/MainActivity.kt
@@ -1,58 +1,59 @@
 package com.smallcake.temp
 
 import android.os.Bundle
+import android.os.Handler
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
 import com.smallcake.smallutils.text.NavigationBar
 import com.smallcake.temp.base.BaseBindActivity
 import com.smallcake.temp.databinding.ActivityMainBinding
+import com.smallcake.temp.fragment.HomeFragment
+import com.smallcake.temp.fragment.ListFragment
+import com.smallcake.temp.fragment.MineFragment
 import com.smallcake.temp.utils.BottomNavUtils
+import com.smallcake.temp.utils.TabNavBottomBean
 import com.smallcake.temp.utils.showToast
 import java.util.*
 
+/**
+ * 申请权限
+private fun checkPermission(){
+XXPermissions.with(this)
+.permission(Permission.RECORD_AUDIO)
+.request(object : OnPermissionCallback {
+override fun onGranted(permissions: MutableList<String>?, all: Boolean) {
+if (all){
 
+}
+}
+override fun onDenied(permissions: MutableList<String>?, never: Boolean) {
+super.onDenied(permissions, never)
+if (never) {
+// 如果是被永久拒绝就跳转到应用权限系统设置页面
+XXPermissions.startPermissionActivity(this@MainActivity, permissions)
+} else {
+showToast("获取定位权限失败")
+}
+}
+})
+}
+ */
 class MainActivity : BaseBindActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?, bar: NavigationBar) {
         bar.hide()
         initView()
-        onEvent()
-        checkPermission()
     }
-
-    private fun onEvent() {
-
-
-    }
-    private fun checkPermission(){
-        XXPermissions.with(this)
-            .permission(Permission.RECORD_AUDIO)
-            .request(object : OnPermissionCallback {
-                override fun onGranted(permissions: MutableList<String>?, all: Boolean) {
-                    if (all){
-
-                    }
-                }
-                override fun onDenied(permissions: MutableList<String>?, never: Boolean) {
-                    super.onDenied(permissions, never)
-                    if (never) {
-                        // 如果是被永久拒绝就跳转到应用权限系统设置页面
-                        XXPermissions.startPermissionActivity(this@MainActivity, permissions)
-                    } else {
-                        showToast("获取定位权限失败")
-                    }
-                }
-            })
-    }
-
     private fun initView() {
-        BottomNavUtils.tabBindViewPager(this,bind.tabLayout,bind.viewPager)
-
+        val tabs = listOf(
+            TabNavBottomBean("首页",R.mipmap.icon_selected_tab1,R.mipmap.icon_unselected_tab1),
+            TabNavBottomBean("列表",R.mipmap.icon_selected_tab2,R.mipmap.icon_unselected_tab2),
+            TabNavBottomBean("我的",R.mipmap.icon_selected_tab3,R.mipmap.icon_unselected_tab3),
+        )
+        val fragments = listOf( HomeFragment(), ListFragment(), MineFragment())
+        BottomNavUtils.initTabNavi(this,bind.tabLayout,bind.viewPager,tabs,fragments)
     }
-
-
-
 
 }
 
--- app/src/main/java/com/smallcake/temp/utils/BottomNavUtils.kt
+++ app/src/main/java/com/smallcake/temp/utils/BottomNavUtils.kt
@@ -2,6 +2,8 @@ package com.smallcake.temp.utils
 
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
@@ -13,6 +15,11 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.smallcake.smallutils.SmallUtils
 import com.smallcake.temp.MyApplication
 import com.smallcake.temp.R
 import com.smallcake.temp.fragment.HomeFragment
@@ -22,120 +29,74 @@ import com.smallcake.temp.fragment.MineFragment
 /**
  * Date: 2020/8/17
  * author: SmallCake
- * 地址导航绑定ViewPager，并装载Fragment
  * 使用：
-BottomNavUtils.tabBindViewPager(this,bind.tabLayout,bind.viewPager)
-BottomNavUtils.setListener(BottomNavListener {when(it){
-0 -> showToast("选中了首页")
-1 -> showToast("选中了列表")
-2 -> showToast("选中了我的")
-} })
+val tabs = listOf(
+TabNavBottomBean("首页",R.mipmap.icon_selected_tab1,R.mipmap.icon_unselected_tab1),
+TabNavBottomBean("列表",R.mipmap.icon_selected_tab2,R.mipmap.icon_unselected_tab2),
+TabNavBottomBean("我的",R.mipmap.icon_selected_tab3,R.mipmap.icon_unselected_tab3),
+)
+val fragments = listOf( HomeFragment(), ListFragment(), MineFragment())
+BottomNavUtils.initTabNavi(this,bind.tabLayout,bind.viewPager,tabs,fragments)
  */
 object BottomNavUtils {
-    private val tabSelectedImgs = intArrayOf(
-        R.mipmap.icon_selected_tab1,
-        R.mipmap.icon_selected_tab2,
-        R.mipmap.icon_selected_tab3
-    )
-    private val tabUnselectedImgs = intArrayOf(
-        R.mipmap.icon_unselected_tab1,
-        R.mipmap.icon_unselected_tab2,
-        R.mipmap.icon_unselected_tab3
-    )
-    private val tabNames = arrayOf("首页", "列表", "我的")
-    private val textColorNormal = ContextCompat.getColor(MyApplication.instance, R.color.gray)
-    private val textColorSelect = ContextCompat.getColor(MyApplication.instance, R.color.black)
-    private var listener : ((Int)->Unit)?=null
-    fun tabBindViewPager(
-        activity: AppCompatActivity,
-        tabLayout: LinearLayout,
-        viewPager: ViewPager
-    ) {
-        val fragments =
-            arrayOf<Fragment>(
-                HomeFragment(),
-                ListFragment(),
-                MineFragment()
-            )
-        viewPager.adapter = object : FragmentStatePagerAdapter(activity.supportFragmentManager, 0) {
-            override fun getCount(): Int {
+    /**
+     * 动态创建Tab
+     * 修改tab_nav_bottom_layout.xml以达到你想要的效果
+     * @param activity AppCompatActivity
+     * @param tabLayout TabLayout
+     * @param viewPager2 ViewPager2
+     * @param tabs List<TabNavBottomBean>
+     * @param fragments List<Fragment>
+     * @param cb Function1<Int, Unit>
+     *
+     * 如果需求：跳转到对应页面可以使用TabLayout的方法
+     * tabLayout.getTabAt(2)?.select()：跳转到第三个页面
+     */
+    fun initTabNavi(activity:AppCompatActivity,tabLayout: TabLayout, viewPager2: ViewPager2,tabs:List<TabNavBottomBean>,fragments:List<Fragment>, cb:((Int)->Unit)?=null){
+        //viewPager2装载Fragment
+        viewPager2.adapter = object :FragmentStateAdapter(activity){
+            override fun getItemCount(): Int {
                 return fragments.size
             }
-
-            override fun getItem(index: Int): Fragment {
-                return fragments[index]
-            }
-
-            override fun getPageTitle(position: Int): CharSequence? {
-                return tabNames[position]
+            override fun createFragment(position: Int): Fragment {
+               return fragments[position]
             }
         }
-        viewPager.offscreenPageLimit = tabNames.size
-        initTabBindViewPager(tabLayout, viewPager)
-    }
+        //最后：tabLayout和ViewPage2联动
+        TabLayoutMediator(tabLayout,viewPager2) { tab, position -> tab.text = tabs[position].name }.attach()
 
-    private fun initTabBindViewPager(
-        tabLayout: LinearLayout,
-        viewPager: ViewPager
-
-    ) {
-        val childCount = tabLayout.childCount
-        for (i in 0 until childCount) {
-            val childAt = tabLayout.getChildAt(i) as LinearLayout
-            childAt.setOnClickListener {
-                toDefaultTab(i, tabLayout, viewPager)
-            }
+        tabLayout.removeAllTabs()
+        //动态添加底部自定义导航栏
+        tabs.forEachIndexed {i,item->
+            val tab = tabLayout.newTab()
+            val view  = LayoutInflater.from(SmallUtils.context).inflate(R.layout.tab_nav_bottom_layout,null)
+            view.findViewById<TextView>(R.id.tv).text = item.name
+            view.findViewById<ImageView>(R.id.iv).setImageResource(if (i==0)item.iconSelect else item.iconUnSelect)
+            tab.customView = view
+            tabLayout.addTab(tab)
         }
-        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrollStateChanged(state: Int) {}
-            override fun onPageScrolled(position: Int,positionOffset: Float,positionOffsetPixels: Int) {}
-            override fun onPageSelected(position: Int) {
-                toDefaultTab(position, tabLayout)
-                listener?.invoke(position)
+        //tabLayout选中事件
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                cb?.invoke(tab.position)
+
+                tabSelectAnim(tab.customView)
+                val position = tab.position
+                val tv = tab.customView?.findViewById<TextView>(R.id.tv)
+                val iv = tab.customView?.findViewById<ImageView>(R.id.iv)
+                tv?.setTextColor(tabs[position].textColorSelect)
+                iv?.setImageResource(tabs[position].iconSelect)
             }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                val position = tab.position
+                val tv = tab.customView?.findViewById<TextView>(R.id.tv)
+                val iv = tab.customView?.findViewById<ImageView>(R.id.iv)
+                tv?.setTextColor(tabs[position].textColorUnSelect)
+                iv?.setImageResource(tabs[position].iconUnSelect)
+            }
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
         })
     }
-
-    /**
-     * Tab选择事件
-     * 1.还原所有Tab为默认状态
-     * 2.选中项加入白色背景和替换为选中图片
-     * 3.联动ViewPager
-     * @param selectPosition 选中项
-     */
-    private fun toDefaultTab(
-        selectPosition: Int,
-        tabLayout: LinearLayout,
-        viewPager: ViewPager? = null
-    ) {
-        //1.还原所有Tab为默认状态
-        for (i in 0 until tabLayout.childCount) {
-            //恢复默认图标
-            val childAt = tabLayout.getChildAt(i) as LinearLayout
-            when (val view = childAt.getChildAt(0)) {
-                is ImageView -> view.setImageResource(tabUnselectedImgs[i])
-                is RelativeLayout -> (view.getChildAt(0) as ImageView).setImageResource(
-                    tabUnselectedImgs[i]
-                )
-            }
-            //恢复默认字体颜色
-            val tvTab = childAt.getChildAt(1) as TextView
-            tvTab.setTextColor(textColorNormal)
-        }
-        //2.选中项加入白色背景和替换为选中图片
-        val childAt = tabLayout.getChildAt(selectPosition) as LinearLayout
-        when (val view = childAt.getChildAt(0)) {
-            is ImageView -> view.setImageResource(tabSelectedImgs[selectPosition])
-            is RelativeLayout -> (view.getChildAt(0) as ImageView).setImageResource(tabSelectedImgs[selectPosition])
-        }
-
-        tabSelectAnim(childAt)
-        //选中字体变黑色
-        (childAt.getChildAt(1) as TextView).setTextColor(textColorSelect)
-        //3.联动ViewPager
-        viewPager?.currentItem = selectPosition
-    }
-
 
     /**
      * tab被选中后的动画部分
@@ -152,10 +113,6 @@ object BottomNavUtils {
         animator.start()
     }
 
-     fun setListener(listener:((Int)->Unit)?){
-        this.listener = listener
-    }
-
-
-
 }
+
+class TabNavBottomBean(val name:String,val iconSelect:Int,val iconUnSelect:Int,val textColorSelect:Int = Color.BLACK,val textColorUnSelect:Int = Color.GRAY)
